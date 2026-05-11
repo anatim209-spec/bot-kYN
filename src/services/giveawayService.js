261c5bac1e5fe529952f65e304e1c09a7afe0744
i7ox67-code <i7ox67@gmail.com>
2026-05-11 22:57:44 +0300

    Update giveawayService.js

--- src/services/giveawayService.js
+++ src/services/giveawayService.js
@@ -179,7 +179,7 @@ export function createGiveawayEmbed(giveaway, status, winners = []) {
         
         const embed = new EmbedBuilder()
             .setTitle(`${statusEmoji} ${giveaway.prize}`)
-            .setDescription('``اضغل بالأفسل للدخول!``')
+            .setDescription('```اضغل بالأفسل للدخول!```')
             .setColor(color)
             .addFields(
                 { name: '👤 المنشئ', value: `<@${giveaway.hostId}>`, inline: true },
